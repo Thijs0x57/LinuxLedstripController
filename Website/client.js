e8832d4098b4ca0b89cb189ff9d16d2f07c97e43
ArthurvanStrien <arthurvanstrien@gmail.com>
2018-03-25 23:57:50 +0200

    Added a functions to the brightness, mode brightness and pattern form.

---
+++ Website/client.js
@@ -0,0 +1,19 @@
+
+
+$(function () {
+
+    $("#patternForm").submit(function (event) {
+        alert("Pattern Form");
+        event.preventDefault();
+    });
+
+    $("#brightnessForm").submit(function (event) {
+        alert("Pattern Form");
+        event.preventDefault();
+    });
+
+    $("#brightnessModeInputForm").submit(function (event) {
+        alert("Pattern Form");
+        event.preventDefault();
+    });
+});
