fb88503d7f204ca5c311bf7994fb803bf0eee204
Zwen1111 <zwen12@msn.com>
2018-03-26 14:30:57 +0200

    Added a function to convert hex color values to RGB values

--- API/index.js
+++ API/index.js
@@ -102,6 +102,22 @@ router.use(function(err, req, res, next) {
     }
 });
 
+// Function for converting hex color values to RGB values
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
 //Finally, start the server application, listening on the given port:
 router.listen(port);
 console.log('Server running at port' + port);
