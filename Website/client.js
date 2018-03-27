cc90d067417e0975e67cd0fa86310bdd0637b074
ArthurvanStrien <arthurvanstrien@gmail.com>
2018-03-27 14:45:50 +0200

    The brightness is now send to the API and messages are shown if it was successfull or not.

--- Website/client.js
+++ Website/client.js
@@ -39,8 +39,33 @@ $(function () {
     });
 
     $("#brightnessForm").submit(function (event) {
-        alert("Pattern Form");
         event.preventDefault();
+
+        var brightness = $("#brightnessForm").find('input[name="brightnessInput"]').val();
+
+        if (brightness > 100)
+            showError("#brightnessError", "The brightness cannot be higher than 100.");
+        else if (brightness < 0)
+            showError("#brightnessError", "The brightness cannot be lower than 0.");
+        else {
+            var data = JSON.stringify({
+                "brightness": brightness
+            });
+
+            $.ajax({
+                method: "POST",
+                url: "/brightness",
+                contentType: "application/json",
+                data: data
+            })
+            .done(function () {
+                showSuccess("#brightnessError", "The brightness was successfully saved");
+                $("#brightnessForm").find('input[name="brightnessInput"]').val("");
+            })
+            .fail(function (msg) {
+                showError("#brightnessError", "Something went wrong, please try again.");
+            });
+        }
     });
 
     $("#brightnessModeInputForm").submit(function (event) {
